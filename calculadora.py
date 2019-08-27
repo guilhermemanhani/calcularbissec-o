07b215fb4ad55ac83595081cc13becc7e38adcff
Guilherme Augusto da Rocha Manhani <guilherme.manhani@iMac-de-Guilherme.local>
2019-08-27 08:59:29 -0300

    calculando valores fixos

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,8 @@
-# from numpy import array
+
+#from numpy import array
+
 x5, x4, x3, x2, x1, x = 0.000, 0.000, 1.000, 0.000, -9.000, 3.000
+calc_intervalo_real_num = []
 interacaoX= []
 interacaoFdeX= []
 interacaoBmenosA= []
@@ -7,10 +10,10 @@ interacaoBmenosA= []
 def calcFdeX(interacao):
     return x5*interacao**5 + x4*interacao**4 + x3*interacao**3 + x2*interacao**2 + x1*interacao + x
 
-def calcularIsolamento(x5, x4, x3, x2, x1, x, intervalo):
+def calcularIsolamento(intervalo):
     resultadoFinal =[]
     for _ in range (intervalo*-2):
-        result = x5*intervalo**5 + x4*intervalo**4 + x3*intervalo**3 + x2*intervalo**2 + x1*intervalo + x
+        result = calcFdeX(intervalo)
         intervalo = intervalo +1
         resultadoFinal.append(result) 
     return resultadoFinal
@@ -21,35 +24,41 @@ def calcularIntervalo(array_resultado, intervalo):
         if (array_resultado[i]<0 and array_resultado[i+1]>0) or (array_resultado[i]>0 and array_resultado[i+1]<0) :
             resultadoFinal.append(intervalo)
             resultadoFinal.append(intervalo+1)
+            calc_intervalo_real_num.append(array_resultado[i])
+            calc_intervalo_real_num.append(array_resultado[i+1])
         intervalo = intervalo+1
     return resultadoFinal
 
-def calcZeroReais (x5, x4, x3, x2, x1, x, a, b):
+def calcZeroReais (a, b):
     resultFdeX = 0
-    interacao = abs((a+b)/2)
-    print(interacao)
-    if interacao < 10**-3:
+    interacao = (a+b)/2
+    criterio_de_parada = abs((b-a)/2)
+    resultFdeX = calcFdeX(interacao)
+    if criterio_de_parada < 10**-3:
         interacaoX.append(interacao)
         return interacaoX
-    resultFdeX = calcFdeX(x5, x4, x3, x2, x1, x, interacao)
-    print(resultFdeX)
+    print(interacao, resultFdeX, criterio_de_parada)
+#     print(interacao)
+#     print(resultFdeX)
     interacaoX.append(interacao) #esse e o resultado do b - a
     interacaoFdeX.append(resultFdeX)
     interacaoBmenosA.append(abs(b-a)/2)
 
     if resultFdeX < 0:
-        calcZeroReais(x5, x4, x3, x2, x1, x, interacao, b)
+        calcZeroReais(a, interacao)
     if resultFdeX > 0:
-        calcZeroReais(x5, x4, x3, x2, x1, x, a, interacao)
+        calcZeroReais(interacao, b)
 
 intervalo = -5
-resultado = calcularIsolamento(x5, x4, x3, x2, x1, x, intervalo)
+resultado = calcularIsolamento(intervalo)
 calc_intervalo = calcularIntervalo(resultado, intervalo)
-zeroReal = calcZeroReais(x5, x4, x3, x2, x1, x, 0, 1)
+zeroReal = calcZeroReais(0, 1)
 # print(interacaoX)
 # print(interacaoFdeX)
 # print(interacaoBmenosA)
-# print(resultado)
-# print(calc_intervalo)
+print(resultado)
+print(calc_intervalo_real_num)
+print(calc_intervalo)
 #print(calcFdeX(x5, x4, x3, x2, x1, x, -5))
+
 
