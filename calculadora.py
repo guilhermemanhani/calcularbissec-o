7c6dc95fd94f903a7182e263f7d232aaaa4a8759
Guilherme Augusto da Rocha Manhani <guilherme.manhani@iMac-de-Guilherme.local>
2019-09-04 09:57:37 -0300

    tratativa do resultado zero

--- calculadora.py
+++ calculadora.py
@@ -12,21 +12,22 @@ def calcFdeX(interacao):
 
 def calcularIsolamento(intervalo):
     resultadoFinal =[]
-    for _ in range (intervalo*-2):
-        result = calcFdeX(intervalo)
-        intervalo = intervalo +1
-        resultadoFinal.append(result) 
+    for i in range(len(intervalo)):
+        result = calcFdeX(intervalo[i])
+        resultadoFinal.append(result)
+        
     return resultadoFinal
 
 def calcularIntervalo(array_resultado, intervalo):
     resultadoFinal =[]
-    for i in range (intervalo*-2-1):
-        if (array_resultado[i]<0 and array_resultado[i+1]>0) or (array_resultado[i]>0 and array_resultado[i+1]<0) :
-            resultadoFinal.append(intervalo)
-            resultadoFinal.append(intervalo+1)
+    # print(array_resultado, intervalo)
+    for i in range(len(intervalo)-1):
+        if array_resultado[i]<1 and array_resultado[i+1]>-1 or array_resultado[i]>-1 and array_resultado[i+1]<1:
+            resultadoFinal.append(intervalo[i])
+            resultadoFinal.append(intervalo[i+1])
             calc_intervalo_real_num.append(array_resultado[i])
             calc_intervalo_real_num.append(array_resultado[i+1])
-        intervalo = intervalo+1
+    # print(resultadoFinal)
     return resultadoFinal
 
 def calcZeroReais (a, b):
@@ -34,15 +35,17 @@ def calcZeroReais (a, b):
     interacao = (a+b)/2
     criterio_de_parada = abs((b-a)/2)
     resultFdeX = calcFdeX(interacao)
-    if criterio_de_parada < 10**-3:
-        interacaoX.append(interacao)
-        return interacaoX
+    # print(a, b)
     print(interacao, resultFdeX, criterio_de_parada)
-#     print(interacao)
+# #     print(interacao)
 #     print(resultFdeX)
     interacaoX.append(interacao) #esse e o resultado do b - a
     interacaoFdeX.append(resultFdeX)
     interacaoBmenosA.append(abs(b-a)/2)
+    if criterio_de_parada < 10**-3:
+        # print(interacao, resultFdeX, criterio_de_parada)
+        interacaoX.append(interacao)
+        return interacaoX
 
     if resultFdeX < 0:
         calcZeroReais(a, interacao)
@@ -53,16 +56,19 @@ def filtraParaCalcular(calc_intervalo_real_num, calc_intervalo):
     tamanho_array = len(calc_intervalo)
     for i in range (0, tamanho_array, 2):
         if calc_intervalo_real_num[i]>0:
-            print(calc_intervalo[i], calc_intervalo[i+1])
+            # print(calc_intervalo[i], calc_intervalo[i+1])
             calcZeroReais(calc_intervalo[i], calc_intervalo[i+1])
         else:
-            print(calc_intervalo[i], calc_intervalo[i+1])
+            # print(calc_intervalo[i], calc_intervalo[i+1])
             calcZeroReais(calc_intervalo[i+1], calc_intervalo[i])
 
-intervalo = -5
+intervalo = list(range(-500, 501))
 resultado = calcularIsolamento(intervalo)
 calc_intervalo = calcularIntervalo(resultado, intervalo)
+# print (calc_intervalo, calc_intervalo_real_num)
 filtraParaCalcular(calc_intervalo_real_num, calc_intervalo)
+
+
 # print(interacaoX)
 # print(interacaoFdeX)
 # print(interacaoBmenosA)
